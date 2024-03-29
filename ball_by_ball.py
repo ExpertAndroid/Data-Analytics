da5e795b571fa926d76e625d239211a28072160f
Shafqat Ullah <shafqatullahkhan5555@gmail.com>
2024-03-29 09:59:52 +0500

    add requirement.txt file for streamlit

--- ball_by_ball.py
+++ ball_by_ball.py
@@ -1,96 +1,98 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pylab as plt
-import seaborn as sns
 
-mydata = pd.read_csv('D:\\Big Data Analytics\\Pythonpractice\\pandas\\Cleaned_ball_by_ball.csv')
-st.title("Ball By Ball IT20 Data")
-st.header("Cricket")
-st.write('Hello Streamlit')
-unique_matches = ''
-st.dataframe(mydata)
+st.write('Hello World')
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pylab as plt
+# import seaborn as sns
 
-
-with st.sidebar:
-    st.image('t20.png', width=150,)
-    st.title('T20 Cricket Data')
+# mydata = pd.read_csv('D:\\Big Data Analytics\\Pythonpractice\\pandas\\Cleaned_ball_by_ball.csv')
+# st.title("Ball By Ball IT20 Data")
+# st.header("Cricket")
+# st.write('Hello Streamlit')
+# unique_matches = ''
+# st.dataframe(mydata)
 
 
-def batFirstandSecondWins():
-    unique_matches = mydata.drop_duplicates(subset='Match ID')
-
-    bat_first_wins = unique_matches[unique_matches['Bat First'] == unique_matches['Winner']].shape[0]
-
-    bat_second_wins = unique_matches[unique_matches['Bat Second'] == unique_matches['Winner']].shape[0]
+# with st.sidebar:
+#     st.image('t20.png', width=150,)
+#     st.title('T20 Cricket Data')
 
 
-    categories = ['Batting First', 'Batting Second']
-    wins_count = [bat_first_wins, bat_second_wins]
+# def batFirstandSecondWins():
+#     unique_matches = mydata.drop_duplicates(subset='Match ID')
 
-    fig, ax = plt.subplots()
-    ax.bar(categories , wins_count, color=['blue', 'yellow'])
-    ax.set_title('Wins by Batting First vs. Batting Second')
-    ax.set_ylabel('Number of Wins')
-    ax.set_xlabel('Batting Team')
-    st.pyplot(fig)
+#     bat_first_wins = unique_matches[unique_matches['Bat First'] == unique_matches['Winner']].shape[0]
 
-def top10winningCountries():
-    unique_countries = mydata.groupby('Match ID')['Winner'].unique().value_counts()
-
-    top_10_countries = unique_countries.head(10)
+#     bat_second_wins = unique_matches[unique_matches['Bat Second'] == unique_matches['Winner']].shape[0]
 
 
-    plot3 = top_10_countries.plot(kind='bar', color=['green', 'blue', 'black', "#90EE90", "yellow", "#ADD8E6", "blue", "brown", "blue", "#90EE90"])
-    plt.title('Top 10 Countries of Most Winnings')
-    plt.xlabel('Country')
-    plt.ylabel('Number of Matches Won')
-    # plt.show()
-    st.pyplot(fig=plot3.figure) 
+#     categories = ['Batting First', 'Batting Second']
+#     wins_count = [bat_first_wins, bat_second_wins]
 
-def chasedScuccessfully():
-    high_target_matches = mydata[mydata['Target Score'] > 200]
+#     fig, ax = plt.subplots()
+#     ax.bar(categories , wins_count, color=['blue', 'yellow'])
+#     ax.set_title('Wins by Batting First vs. Batting Second')
+#     ax.set_ylabel('Number of Wins')
+#     ax.set_xlabel('Batting Team')
+#     st.pyplot(fig)
 
-    unique_matches3 = high_target_matches.groupby('Match ID')['Chased Successfully'].sum()
-    won = (unique_matches3 > 0).sum()
-    lost = (unique_matches3 == 0).sum()
+# def top10winningCountries():
+#     unique_countries = mydata.groupby('Match ID')['Winner'].unique().value_counts()
 
-    fig, ax = plt.subplots()
-    ax.bar(['Successfully Chased', 'Unsuccessfully Chased'], [won, lost], color=['green', 'red'])
-    ax.set_title('Matches Result with Target Score > 200')
-    ax.set_ylabel('Number of Matches')
-    ax.set_xlabel('Result')
-    st.pyplot(fig)
+#     top_10_countries = unique_countries.head(10)
 
-def mostSuccessfullChasedGrounds():
-   unique_matches = mydata.drop_duplicates(subset='Match ID')
 
-   unique_data = unique_matches.groupby('Match ID').agg({
-    "Chased Successfully" : "first",
-    "Venue" : "first"
-   }).reset_index()
+#     plot3 = top_10_countries.plot(kind='bar', color=['green', 'blue', 'black', "#90EE90", "yellow", "#ADD8E6", "blue", "brown", "blue", "#90EE90"])
+#     plt.title('Top 10 Countries of Most Winnings')
+#     plt.xlabel('Country')
+#     plt.ylabel('Number of Matches Won')
+#     # plt.show()
+#     st.pyplot(fig=plot3.figure) 
 
-   num_words_to_keep = 2 
-   unique_data['Abbreviated Ground'] = unique_data['Venue'].str.split().str.slice(0, num_words_to_keep).str.join(' ')
+# def chasedScuccessfully():
+#     high_target_matches = mydata[mydata['Target Score'] > 200]
 
-   chased_data = unique_data[unique_data['Chased Successfully'] == 1]
+#     unique_matches3 = high_target_matches.groupby('Match ID')['Chased Successfully'].sum()
+#     won = (unique_matches3 > 0).sum()
+#     lost = (unique_matches3 == 0).sum()
 
-   chased_score_grounds = chased_data['Abbreviated Ground'].value_counts().head(10)
+#     fig, ax = plt.subplots()
+#     ax.bar(['Successfully Chased', 'Unsuccessfully Chased'], [won, lost], color=['green', 'red'])
+#     ax.set_title('Matches Result with Target Score > 200')
+#     ax.set_ylabel('Number of Matches')
+#     ax.set_xlabel('Result')
+#     st.pyplot(fig)
 
-   colors = ['green', 'yellow', 'blue', 'skyblue', 'red']
+# def mostSuccessfullChasedGrounds():
+#    unique_matches = mydata.drop_duplicates(subset='Match ID')
 
-   plot4 = chased_score_grounds.plot(kind='bar', color=colors)
+#    unique_data = unique_matches.groupby('Match ID').agg({
+#     "Chased Successfully" : "first",
+#     "Venue" : "first"
+#    }).reset_index()
 
-   # plot4 = sns.barplot(x=chased_score_grounds.index, y=chased_score_grounds.values, palette=colors)
-   plt.title('Top 10 Grounds with Most Successful Chases')
-   plt.xlabel('Ground')
-   plt.ylabel('Number of Successful Chases')
-   plt.xticks(rotation=90)
-   st.pyplot(fig=plot4.figure)  
+#    num_words_to_keep = 2 
+#    unique_data['Abbreviated Ground'] = unique_data['Venue'].str.split().str.slice(0, num_words_to_keep).str.join(' ')
 
-# mostSuccessfullChasedGrounds()
-# top10winningCountries()
-# batFirstandSecondWins()
-# chasedScuccessfully()   
+#    chased_data = unique_data[unique_data['Chased Successfully'] == 1]
+
+#    chased_score_grounds = chased_data['Abbreviated Ground'].value_counts().head(10)
+
+#    colors = ['green', 'yellow', 'blue', 'skyblue', 'red']
+
+#    plot4 = chased_score_grounds.plot(kind='bar', color=colors)
+
+#    # plot4 = sns.barplot(x=chased_score_grounds.index, y=chased_score_grounds.values, palette=colors)
+#    plt.title('Top 10 Grounds with Most Successful Chases')
+#    plt.xlabel('Ground')
+#    plt.ylabel('Number of Successful Chases')
+#    plt.xticks(rotation=90)
+#    st.pyplot(fig=plot4.figure)  
+
+# # mostSuccessfullChasedGrounds()
+# # top10winningCountries()
+# # batFirstandSecondWins()
+# # chasedScuccessfully()   
 
 
